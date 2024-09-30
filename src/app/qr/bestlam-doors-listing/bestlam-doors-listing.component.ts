f19279bff7a8137d429fdbeab940615c36639a20
ashwinlimbanee <ashwinlimbani23@gmail.com>
2024-09-30 20:53:50 +0530

    show table

--- src/app/qr/bestlam-doors-listing/bestlam-doors-listing.component.ts
+++ src/app/qr/bestlam-doors-listing/bestlam-doors-listing.component.ts
@@ -5,8 +5,39 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './bestlam-doors-listing.component.html',
-  styleUrl: './bestlam-doors-listing.component.scss'
+  styleUrl: './bestlam-doors-listing.component.scss',
 })
 export class BestlamDoorsListingComponent {
-
+  doorDesigns = [
+    {
+      designNumber: 100,
+      name: 'Classic Oak',
+      img: './assets/bestlam/100.png',
+    },
+    {
+      designNumber: 101,
+      name: 'Classic Oak',
+      img: './assets/bestlam/101.png',
+    },
+    {
+      designNumber: 102,
+      name: 'Classic Oak',
+      img: './assets/bestlam/102.png',
+    },
+    {
+      designNumber: 103,
+      name: 'Classic Oak',
+      img: './assets/bestlam/103.png',
+    },
+    {
+      designNumber: 104,
+      name: 'Classic Oak',
+      img: './assets/bestlam/104.png',
+    },
+    {
+      designNumber: 105,
+      name: 'Classic Oak',
+      img: './assets/bestlam/105.png',
+    },
+  ];
 }
