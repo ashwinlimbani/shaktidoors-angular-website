892440aa927a8642f305f8019dc2ed8a6ee14759
ashwinlimbanee <ashwinlimbani23@gmail.com>
2025-06-02 15:47:36 +0530

    copy gallery

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { LightboxModule } from 'ngx-lightbox';
+import { importProvidersFrom } from '@angular/core';
 import { provideLottieOptions } from 'ngx-lottie';
 import player from 'lottie-web';
 
@@ -9,6 +11,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(LightboxModule),
     provideLottieOptions({
       player: () => player,
     }),
--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,44 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { Lightbox, LightboxModule } from 'ngx-lightbox';
-
-interface IAlbum {
-  src: string;
-  caption?: string;
-  thumb: string;
-}
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
-  standalone: true,
-  imports: [CommonModule, RouterLink, LightboxModule],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss',
+  styleUrls: ['./gallery.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
 })
-export class GalleryComponent implements OnInit {
-  album: IAlbum[] = [];
-
-  constructor(private _lightbox: Lightbox) {
-    // Add all gallery images
-    for (let i = 1; i <= 52; i++) {
-      const src = `assets/images/${i.toString().padStart(2, '0')}.jpg`;
-      const thumb = src;
-      const album = {
-        src: src,
-        thumb: thumb,
-      };
-      this.album.push(album);
-    }
-  }
-
-  ngOnInit(): void {}
-
-  open(index: number): void {
-    this._lightbox.open(this.album, index);
-  }
-
-  close(): void {
-    this._lightbox.close();
-  }
-}
+export class GalleryComponent {}
